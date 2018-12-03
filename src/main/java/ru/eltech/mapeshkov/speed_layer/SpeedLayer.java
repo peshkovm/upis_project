6d6ad30c823ac1605465b933ab98114456261a92
Maksim Peshkov <peshmaxan@gmail.com>
2018-12-03 16:56:32 +0300

    Add speed layer with Alpha Vantage Stock Time intraday and Cryptocurrencies Exchange Rates api

---
+++ src/main/java/ru/eltech/mapeshkov/speed_layer/SpeedLayer.java
@@ -0,0 +1,23 @@
+package ru.eltech.mapeshkov.speed_layer;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SpeedLayer {
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(
+                ApiUtils.AlphaVantage::parseDigitalAndCryptoCurrencies
+                , 0L
+                , 2L
+                , TimeUnit.SECONDS
+        );
+        scheduler.schedule(scheduler::shutdown, 10, TimeUnit.SECONDS);
+        try {
+            scheduler.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
